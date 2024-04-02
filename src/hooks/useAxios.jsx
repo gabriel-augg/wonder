6abb3063858dedb2f1d4f1d9a22b5ce62dbf2077
Gabriel <gabrielgapseu@gmail.com>
2024-04-02 09:20:03 -0300

    Refactor: alteration on axios hooks

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import api from '../utils/api';
 
-const useAxios = (url) => {
+const useAxios = () => {
     const [loading, setLoading] = useState(true);
+    const [loadingSubmit, setLoadingSubmit] = useState(false)
 
-    const getPosts = async (options = {}) => {
+    const get = async (url, options = {}) => {
         try {
             const data = await api.request({
                 url,
@@ -19,7 +20,23 @@ const useAxios = (url) => {
         }
     }
 
-    return { getPosts, loading };
+    const create = async (url, data, options = {}) => {
+        setLoadingSubmit(true)
+        try {
+            const data = await api.request({
+                url, 
+                data, 
+                ...options
+            })
+            return data
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoadingSubmit(false)
+        }
+    }
+
+    return { get, create, loading, loadingSubmit };
 };
 
 export default useAxios;
