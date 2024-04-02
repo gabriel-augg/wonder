6747f59b7860ab58dcbbb7f32188985a9214b11f
Gabriel <gabrielgapseu@gmail.com>
2024-04-02 08:49:32 -0300

    Refactor: alteration on home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,41 +12,50 @@ import styles from "./styles.module.css"
 import image_no_posts from "../../assets/img/ilustration_no_posts.svg"
 
 import { AiOutlineLoading } from "react-icons/ai";
+import useAxios from "../../hooks/useAxios.jsx";
 
 
 export default function Home(){
+    const { getPosts, loading } = useAxios("/posts")
+
     const [posts, setPosts] = useState([])
     const {search} = useContext(SearchContext)
     const [offset, setOffSet] = useState(0)
-    const [loading, setLoading] = useState(true)
     const [loadingMore, setLoadingMore] = useState(false)
+    const [isPostsEmpty, setIsPostsEmpty] = useState(false)
 
     useEffect(()=>{
-        const findPosts = () => {
-            api.get(`/posts?search=${search}`).then((res) => {
-                setPosts(res.data.posts)
-                setLoading(false)
+            const options = {
+                method: "get",
+                params: {
+                    offset,
+                    ...(search && {search})
+                }
+            }
+
+            getPosts(options)
+            .then(({data}) => {
+                if (offset === 0) {
+                    setPosts(data.posts)
+                } else {
+                    setPosts(prevPosts => [...prevPosts, ...data.posts])
+                    setLoadingMore(false)
+                }
+                if(data.posts.length === 0){
+                    setIsPostsEmpty(true)
+                }
             })
-            .catch(()=>{
-                setLoading(false)
+            .catch((error) => {
+                console.log(error)
             })
-        }
 
-        findPosts()
+    },[offset, search])
 
-    },[search, true])
-
-    function handleFindPosts(){
-        setLoadingMore(true)
-        api.get(`/posts?search=${search}&offset=${offset + 5}`).then((res) => {
-            setPosts([...posts, ...res.data.posts])
-            setOffSet(offset + 5)
-            setLoadingMore(false)
-        })
-        .catch(()=>{
-            setLoadingMore(false)
-        })
+    function handleBtnMore(){
+        setOffSet(prevOffSet => prevOffSet + 5)
+        loadingMore(true)
     }
+
 
     if(loading){
         return(
@@ -69,27 +78,37 @@ export default function Home(){
                 <ButtonCta title="+ Nova postagem" path="/novapostagem"/>
             </Title>
             <div>
-                {posts.length === 0 ? (
-                    <div className={styles.no_post_container}>
-                        <h1>Não encontramos nenhum post no momento</h1>
-                        <img src={image_no_posts} alt="ilustration" />
-                    </div>
-                ) : (
+                {posts.length > 0 ? (
                     <>
                         <div className={styles.post_area}>
                             {posts.map(post => {
                                 return (
-                                    <Post id={post.id} username={post["User.username"]} createdAt={post.createdAt} likesQty={post.liked} txt={post.description
-                                    } answerQty={post.answer_qty} type="posts" />
+                                    <Post 
+                                        key={post.id} 
+                                        id={post.id} 
+                                        username={post["User.username"]} 
+                                        createdAt={post.createdAt} 
+                                        likesQty={post.liked} 
+                                        txt={post.description} 
+                                        answerQty={post.answer_qty} 
+                                        type="posts" 
+                                    />
                                 )
                             })}
                         </div>
-                        <div className={styles.find_more}>
-                            <button onClick={handleFindPosts}>
-                                { loadingMore ? <AiOutlineLoading size={20}/> : "Buscar mais"}
-                            </button>
-                        </div>
+                        { !isPostsEmpty && (
+                            <div className={styles.find_more}>
+                                <button onClick={handleBtnMore}>
+                                    { loadingMore ? <AiOutlineLoading size={20}/> : "Buscar mais"}
+                                </button>
+                            </div>
+                        )}
                     </>
+                ) : (
+                    <div className={styles.no_post_container}>
+                        <h1>Não encontramos nenhum post no momento</h1>
+                        <img src={image_no_posts} alt="ilustration" />
+                    </div>
                 )}
             </div>
         </section>
