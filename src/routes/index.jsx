2113457701056fbbbee2b39b742f4fec8ea3f74f
Gabriel <gabrielgapseu@gmail.com>
2024-04-12 21:10:02 -0300

    Fix: bugfix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,6 +9,7 @@ import ShowUserPosts from "../pages/ShowUserPosts"
 import PostEdit from "../pages/PostEdit"
 import Profile from "../pages/Profile"
 import ViewLikedPosts from "../pages/ViewLikedPosts"
+import EditUser from "../pages/EditUser"
 
 import NotFound from "../pages/NotFound"
 import Private from "./Private"
@@ -19,12 +20,14 @@ export default function AppRoutes(){
         <Routes>
             <Route path="/entrar" element={ <SignIn/> } />
             <Route path="/cadastrar" element={ <SignUp/> } />
+            <Route path="/perfil/:id" element={<Profile />} />
             <Route path="/nova-publicacao" element={ <Private><CreatePost/></Private> } />
             <Route path="/publicacoes/:id" element={<Private><ViewPost/></Private>} />
             <Route path="/minhas-publicacoes" element={<Private><ShowUserPosts/></Private>} />
             <Route path="/minhas-curtidas" element={<Private><ViewLikedPosts/></Private>} />
             <Route path="/publicacoes/editar/:id" element={<Private><PostEdit/></Private>} />
-            <Route path="/perfil/:id" element={<Profile />} />
+            <Route path="/perfil/editar/meu-perfil" element={<Private><EditUser/></Private>} />
+
             <Route path="/" element={ <Home /> } />
 
             <Route path="*" element={<NotFound/>} />
